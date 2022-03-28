298974758bfca6ea40101a5d983209cfd8509a2e
Dee <ajalaoyinda@gmail.com>
2022-03-28 20:01:20 +0500

    new changes

--- src/components/Effect.js
+++ src/components/Effect.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {useEffect, useState} from "react"
 import axios from "axios"
-import useAxios from "./useAxios"
 import "../index.css"
 import img1 from "../Wallpapers/w1.jpg"
 import img2 from "../Wallpapers/w2.webp"
@@ -9,7 +8,6 @@ import img3 from "../Wallpapers/w3.jpg"
 import img4 from "../Wallpapers/w4.jpg"
 import img5 from "../Wallpapers/w5.jpg"
 import img6 from "../Wallpapers/w6.jpg"
-import img7 from "../Wallpapers/w7.jpg"
 import img8 from "../Wallpapers/w8.jpg"
 import img9 from "../Wallpapers/w9.jpg"
 import img10 from "../Wallpapers/w10.webp"
@@ -17,34 +15,46 @@ import img11 from "../Wallpapers/w11.jpg"
 import img12 from "../Wallpapers/w12.jpg"
 import img13 from "../Wallpapers/w13.jpg"
 import Container from "./Container"
-
+const API_KEY= process.env.REACT_APP_API_KEY
 
 var dataa
 export default function Effect() {
+    useEffect(() => {
+        const showSlides=()=> {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}
+            slides[slideIndex-1].style.display = "block";
+            setTimeout(showSlides, 5000);
+        }
+        showSlides()
+    }, [])
 
-    const urll="https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=31fe60a104e32be3553f171cf4fc0053"
-    const [num, setNum ] = useState(0)
+    const [loc, setLoc] = useState('London')
+
+
     const [long, setLong ] = useState(0)
     const [lati, setLati ] = useState(0)
     const [nameof, setNameof] = useState('')
     const [tempof, setTempof] = useState(0)
     const [desc, setDesc] = useState('')
+
     const [ctempof, setcTempof] = useState(0)
         let slideIndex = 0;
+ 
 
-        
         const check=()=>{
-            const options = {
-                method: 'GET',
-                url: "https://api.openweathermap.org/data/2.5/weather?q=ogbomoso&appid=31fe60a104e32be3553f171cf4fc0053"
-            };
-    
-              axios.request(options).then(function (response) {
+            var locationn=prompt("State your location")
+            setLoc(locationn)
+        const urll= `https://api.openweathermap.org/data/2.5/weather?q=${locationn}&appid=${API_KEY}`
+              axios.get(urll).then(function (response) {
                   let dataa=(response.data)
                   let dataaa=(response.data.main)
                   let datade=(response.data.weather[0].description)
-                  
-                  console.log(dataa);
                   setNameof(dataa.name)
                   setTempof(dataaa.temp)
                   setDesc(datade)
@@ -52,17 +62,18 @@ export default function Effect() {
                   
                   
                 }).catch(function (error) {
-                    console.error(error.message);
+                    console.error(error.message);  
                 });
             }
+
         const getLocation=()=>{
 
             if(navigator.geolocation){
                 navigator.geolocation.getCurrentPosition((response)=>{
-                    console.log(response.coords.latitude)
-                console.log(response.coords.longitude)            
-                setLong(response.coords.longitude)
-                setLati(response.coords.latitude) 
+                //     console.log(response.coords.latitude)
+                // console.log(response.coords.longitude)            
+                // setLong(response.coords.longitude)
+                // setLati(response.coords.latitude) 
                 check()
             })
 
@@ -75,23 +86,7 @@ export default function Effect() {
         
     }
 
-        
-        const showSlides=()=> {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) {slideIndex = 1}
-            slides[slideIndex-1].style.display = "block";
-            setTimeout(showSlides, 6000);
-        }
-        showSlides();
-
-
-
-
+           
         return (
             <>
  {/* <div>{tempof}*C</div> */}
@@ -102,14 +97,12 @@ export default function Effect() {
         <button onClick={check}>Check weather</button> */}
         <div className="slideshow-container">
 
-
 <Container dynimage={img1} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
 <Container dynimage={img2} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
 <Container dynimage={img3} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
 <Container dynimage={img4} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
 <Container dynimage={img5} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
 <Container dynimage={img6} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
-{/* <Container dynimage={img7} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container> */}
 <Container dynimage={img8} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
 <Container dynimage={img9} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
 <Container dynimage={img10} cctempof={ctempof} cdesc={desc} cnameof={nameof} cgetLocation={getLocation}></Container>
@@ -119,6 +112,5 @@ export default function Effect() {
   </div>
        </>
 )
-
 
 }
--- src/components/useAxios.js
+++ src/components/useAxios.js
@@ -1,9 +1,9 @@
-import{useState, useEffect} from "react"
-import axios from "axios"
+// import{useState, useEffect} from "react"
+// import axios from "axios"
+// const url= "https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=31fe60a104e32be3553f171cf4fc0053"
 
+// const useAxios=(url)=>{
 
-const useAxios=(urll)=>{
+// }
 
-}
-
-export default useAxios;
+// export default useAxios;
